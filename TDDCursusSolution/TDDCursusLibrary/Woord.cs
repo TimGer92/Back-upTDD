138184785d9db32947587fd92b03a3800c4515be
Timmy Gerits <geritstimmy@hotmail.com>
2018-03-27 12:30:13 +0200

    Veiling

---
+++ TDDCursusSolution/TDDCursusLibrary/Woord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCursusLibrary
+{
+
+    public class Woord
+    {
+        private readonly string woord;
+        public Woord(string woord)
+        {
+            this.woord = woord;
+        }
+
+        public bool IsPalindroom()
+        {
+            var omgekeerd = new string(woord.ToArray().Reverse().ToArray());
+            return woord == omgekeerd;
+        }
+
+
+
+    }
+}
---
+++ TDDCursusSolution/TDDCursusLibraryTest/WoordTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDCursusLibrary;
+
+namespace TDDCursusLibraryTest
+{
+
+    [TestClass]
+    public class WoordTest
+    {
+        [TestMethod]
+        public void LepelIsPalindroom()
+        {
+            Assert.IsTrue(new Woord("lepel").IsPalindroom());            
+        }
+
+        [TestMethod]
+        public void AppelIsGeenPalindroom()
+        {
+            Assert.IsFalse(new Woord("appel").IsPalindroom());
+        }
+    }
+}
